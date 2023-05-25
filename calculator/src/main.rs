1df026cccd82bfca08522e646a21c32461d2ccba
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-05-25 10:03:35 -0500

    commit

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
     
-    let mut number1;
-    let mut number1;
-    let mut operator;
+    let mut number1: String = String::new();
+    let mut number1: String = String::new();
+    let mut operator: String = String::new();
+
+    println!("Please enter the first number");
+    std::io::stdin().read_line(&mut number1 ).expect("Failed to read first number.");
+
+    println!("Please enter the second number");
+    std::io::stdin().read_line(&mut number2 ).expect("Failed to read second number.");
+
+    println!("Please enter the operator for addition, subtraction, multiplication, division, or exponents.")
+    std::io::stdin().read_line(&mut operator).expect("failed to read operator input.")
+
 }
 
 //We need to read two numbers from the user.
