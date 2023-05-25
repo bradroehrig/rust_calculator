cbf00e27d181d05ca90212b5dd7e02769480da00
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-05-25 14:54:44 -0500

    commit

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,56 +1,13 @@
 fn main() {
-    
-    let mut number1: String = String::new();
-    let mut number2: String = String::new();
-    let mut operator: String = String::new();
 
-    println!("Please enter the first number");
-    std::io::stdin()
-        .read_line(&mut number1 )
-        .expect("Failed to read first number.");
+    let num_list = [1,2,3,4,5];
+    let months = ["January", "February", "March"];
 
-    println!("Please enter the second number");
-    std::io::stdin()
-        .read_line(&mut number2 )
-        .expect("Failed to read second number.");
+    let num_list2:[i32; 5] = [1,2,3,4,5];
 
-    println!("Please enter the operator for addition, subtraction, multiplication, division, or exponents.");
-    
-    std::io::stdin()
-        .read_line(&mut operator)
-        .expect("failed to read operator input.");
+    let same = ["Hi"; 10];
 
-    let num1 :u32 = number1
-        .trim()
-        .parse()
-        .expect("Failed to convert number 1, make sure it is a valid number.");
-
-    let num2 :u32 = number2
-        .trim()
-        .parse()
-        .expect("failed to convert number 2, make sure it is a valid number.");
-
-    if operator.trim()=="+" {
-        println!("The sum of {} and {} is {}.", num1, num2, num1+num2);
-    }
-
-    else if operator.trim()=="-"{
-        println!("The subtraction of {} and {} is {}.", num1, num2, num1-num2);
-    }
-
-    else if operator.trim()=="*"{
-        println!("The multiplication of {} and {} is {}.", num1, num2, num1*num2);
-    }
-    else if operator.trim()=="/"{
-        println!("The division of {} and {} is {}.", num1, num2, num1/num2);
-    }
-    else{
-        println!("You input something incorrectly!")
-    }
+    println!("{}", num_list[0]);
+    println!("{}", same[9]);   
 
 }
-
-//We need to read two numbers from the user.
-//We need to read the operation - addition, subtraction, multiplication or division, exponents.
-//We need to perform that operation depending upon the choice of operation by the user.
-//Display the results
