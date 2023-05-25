73c6dd9e78f58a9ef882c8cb57b998a345852dcf
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-05-25 15:52:04 -0500

    commit

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,17 +1,12 @@
 fn main() {
 
     let a = [1,2,3,4,5];
-    let b = ["hi","hi","hi","hi","hi"];
-    let mut index = 0;
-    let mut hi = 0;
 
-    while index<5 {
-        println!("{}",a[index]);
-        index = index + 1;
+    let b = a.iter();
+
+    for i in b {
+        println!("{}", i);
+
     }
 
-    while hi<5 {
-        println!("{}",b[hi]);
-        hi = hi + 1;
-    }
 }
